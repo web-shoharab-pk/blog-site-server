eb97c78b115a647f4aaaae5185391812f062d448
md shoharab pk <shoharabpk.2019@gmail.com>
2021-06-08 09:18:37 +0600

    post method done

--- index.js
+++ index.js
@@ -12,13 +12,61 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const blogCollection = client.db(`${process.env.DB_DATA}`).collection(`${process.env.DB_BLOG_COLLECTION}`);
+  const adminCollection = client.db(`${process.env.DB_DATA}`).collection(`${process.env.DB_ADMIN_COLLECTION}`);
+  const userCollection = client.db(`${process.env.DB_DATA}`).collection(`${process.env.DB_USER_COLLECTION}`);
+  const writerCollection = client.db(`${process.env.DB_DATA}`).collection(`${process.env.DB_CONTENT_WEITER_COLLECTION}`);
   app.get('/', (req, res) => {
       console.log(uri)
     res.send('Hello World!')
   })
-  // perform actions on the collection object
+
+  app.post('/addBlogsWriter', (req, res) => {
+    const data = req.body;
+    writerCollection.insertOne(data)
+    .then((result) => {
+      console.log(result)
+      res.send(result.insertedCount > 0)
+    })
+    console.log(data)
+  })
+
+  app.post('/addanAdmin', (req, res) => {
+    const data = req.body;
+    adminCollection.insertOne(data)
+    .then((result) => {
+      console.log(result)
+      res.send(result.insertedCount > 0)
+    })
+    console.log(data)
+  }) 
   
+
+  app.post('/addBlog', (req, res) => {
+    const data = req.body;
+    blogCollection.insertOne(data)
+    .then((result) => {
+      console.log(result)
+      res.send(result.insertedCount > 0)
+    })
+    console.log(data)
+  }) 
+
+
+  app.post('/collectUsers', (req, res) => {
+    const data = req.body;
+    userCollection.insertOne(data)
+    .then((result) => {
+      console.log(result)
+      res.send(result.insertedCount > 0)
+    })
+    console.log(data)
+  }) 
+
+
+
 });
+
+
 
 
 
