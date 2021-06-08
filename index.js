c4daa786a63c74efda909da584ee1f90cf2facd9
md shoharab pk <shoharabpk.2019@gmail.com>
2021-06-08 20:58:47 +0600

    jwt complete

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
+const admin = require('firebase-admin');
 const MongoClient = require('mongodb').MongoClient;
 const port = 5500
 require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
+
+
+var serviceAccount = require("./blog-site-38a73-firebase-adminsdk-b813g-197e76ba77.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qkzne.mongodb.net/${process.env.DB_DATA}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,7 +27,7 @@ client.connect(err => {
   const writerCollection = client.db(`${process.env.DB_DATA}`).collection(`${process.env.DB_CONTENT_WEITER_COLLECTION}`);
   app.get('/', (req, res) => {
     console.log(uri)
-    res.send('Hello World!')
+    res.send('Hello World! I am a blogsite api')
   })
 
   app.post('/addBlogsWriter', (req, res) => {
@@ -27,7 +37,6 @@ client.connect(err => {
         console.log(result)
         res.send(result.insertedCount > 0)
       })
-    console.log(data)
   })
 
   app.post('/addanAdmin', (req, res) => {
@@ -37,7 +46,6 @@ client.connect(err => {
         console.log(result)
         res.send(result.insertedCount > 0)
       })
-    console.log(data)
   })
 
 
@@ -45,21 +53,18 @@ client.connect(err => {
     const data = req.body;
     blogCollection.insertOne(data)
       .then((result) => {
-        console.log(result)
         res.send(result.insertedCount > 0)
       })
-    console.log(data)
   })
 
 
   app.post('/collectUsers', (req, res) => {
     const data = req.body;
-    userCollection.insertOne(data)
-      .then((result) => {
-        console.log(result)
-        res.send(result.insertedCount > 0)
-      })
-    console.log(data)
+    // userCollection.insertOne(data)
+    //   .then((result) => {
+    //     console.log(result)
+    //     res.send(result.insertedCount > 0)
+    //   }) 
   })
 
 
@@ -69,17 +74,53 @@ client.connect(err => {
       .toArray((err, admin) => {
         res.send(admin.length > 0)
       })
-    console.log(email)
   })
 
 
   app.post('/isWriter', (req, res) => {
     const email = req.body.email;
-    adminCollection.find({ email: email })
+    writerCollection.find({ email: email })
       .toArray((err, writer) => {
         res.send(writer.length > 0)
       })
-    console.log(email)
+  })
+
+
+  app.get('/allBlogs', (req, res) => {
+    blogCollection.find({})
+      .toArray((err, blogs) => {
+        res.send(blogs)
+      })
+  })
+
+
+  app.get('/myBlogs', (req, res) => {
+    const bearer = req.headers.authorization
+    console.log(req.query.email)
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const jwtToken = bearer.split(' ')[1]
+      // console.log(jwtToken)
+      admin
+        .auth()
+        .verifyIdToken(jwtToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          console.log(tokenEmail)
+          if(tokenEmail == req.query.email) {
+            blogCollection.find({blogerEmail: req.query.email})
+            .toArray((err, result) => {
+              res.send(result)
+            })
+          }
+
+          // ...
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    }
+
+
   })
 
 
